14e45048381efc083dd72d3531c64cfa96e0b97f
anmolk3797 <anmol899a.s@gmail.com>
2023-02-21 12:56:09 +0530

    leave-tracker-frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
+import { Route, Routes } from 'react-router-dom';
+import MainLayout from './component/common/MainLayout';
+import { Dashboard } from './component/pages/dashboard';
+import  {LeaveForm}  from './component/pages/leaveform';
+import SignIn from './component/pages/signin';
+import SignUp from './component/pages/signup/Signup';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainLayout/>
+      <Routes>
+        <Route path="/" element={<Dashboard />}/>
+        <Route path="/login" element={<SignIn />}/>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/leaveform" element={<LeaveForm />} />
+      </Routes>
     </div>
   );
 }
